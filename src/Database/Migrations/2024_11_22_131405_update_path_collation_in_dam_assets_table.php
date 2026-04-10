6bc865869974b32e882ff1cedb6eeea6e01cc24d
vipinkutthi-webkul <vipinkutthi.akeneo311@webkul.in>
2026-04-10 13:14:31 +0530

    pgsql migration issue with collation resolve

--- src/Database/Migrations/2024_11_22_131405_update_path_collation_in_dam_assets_table.php
+++ src/Database/Migrations/2024_11_22_131405_update_path_collation_in_dam_assets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,6 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (DB::getDriverName() !== 'mysql') {
+            return;
+        }
+
         Schema::table('dam_assets', function (Blueprint $table) {
             $table->string('path')->collation('utf8mb4_bin')->change();
         });
@@ -21,6 +26,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        if (DB::getDriverName() !== 'mysql') {
+            return;
+        }
+
         Schema::table('dam_assets', function (Blueprint $table) {
             $table->string('path')->collation('utf8mb4_unicode_ci')->change();
         });
