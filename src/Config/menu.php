2e5b972d00b65db1423ccfa374dba4b08d673455
vipinkutthi-webkul <vipinkutthi.akeneo311@webkul.in>
2026-05-20 21:30:18 +0530

    fix(dam): redirect DAM menu items to assets index

--- src/Config/menu.php
+++ src/Config/menu.php
@@ -4,14 +4,14 @@ return [
     [
         'key'   => 'dam',
         'name'  => 'dam::app.admin.components.layouts.sidebar.dam',
-        'route' => 'admin.dam.index',
+        'route' => 'admin.dam.assets.index',
         'sort'  => 10,
         'icon'  => 'icon-dam-menu',
     ],
     [
         'key'   => 'dam.assets',
         'name'  => 'dam::app.admin.components.layouts.sidebar.assets',
-        'route' => 'admin.dam.index',
+        'route' => 'admin.dam.assets.index',
         'sort'  => 1,
         'icon'  => '',
     ],
