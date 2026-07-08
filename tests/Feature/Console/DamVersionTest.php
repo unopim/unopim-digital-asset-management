f4514db254683fba74b2ebaaaa7f23b4f2c53bbd
Prince <prince.sahni322@webkul.in>
2026-07-08 15:30:22 +0530

    Feat: remove the version check

--- tests/Feature/Console/DamVersionTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-it('prints the installed DAM version from the changelog', function () {
-    $this->artisan('dam:version')
-        ->expectsOutputToContain('3.0')
-        ->assertSuccessful();
-});
